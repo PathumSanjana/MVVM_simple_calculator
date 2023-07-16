98c3818377c60ab8e01204d36ded3661f337b067
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-07-16 14:08:43 +0530

    multifyButton view added

--- app/src/main/java/com/example/mvvm_application/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/mvvm_application/ui/main/MainFragment.kt
@@ -61,6 +61,14 @@ class MainFragment : Fragment() {
             viewModel.sub(num1, num2)
         }
 
+        var multifyButton = view.findViewById<Button>(R.id.multifyButton)
+        multifyButton.setOnClickListener{
+            //viewModel.multify(4.0,6.0)
+            val num1 = editText1.text.toString().toDouble()
+            val num2 = editText2.text.toString().toDouble()
+            viewModel.multify(num1, num2)
+        }
+
         return view
     }
 
